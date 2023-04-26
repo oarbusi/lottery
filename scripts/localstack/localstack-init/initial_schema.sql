0b5c4f2b65dfb232423d0db0ad7e78284c7efabe
Oriol <oriol.arbusi@glovoapp.com>
2023-04-26 18:07:28 +0200

    set up docker-compose.yaml to run localstack and create database

---
+++ scripts/localstack/localstack-init/initial_schema.sql
@@ -0,0 +1,10 @@
+CREATE TABLE users (
+   id INT PRIMARY KEY,
+   name VARCHAR(255),
+   email VARCHAR(255)
+);
+
+INSERT INTO users (id, name, email) VALUES (1, 'Alice', 'alice@example.com');
+INSERT INTO users (id, name, email) VALUES (2, 'Bob', 'bob@example.com');
+
+
